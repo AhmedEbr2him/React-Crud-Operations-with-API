528b91fe2b5a6d97e2ee979f29140beb202f1249
AhmedEbrahim <89828478+AhmedEbr2him@users.noreply.github.com>
2024-06-23 06:40:32 +0300

    add json server

--- src/Users/Users.jsx
+++ src/Users/Users.jsx
@@ -13,13 +13,17 @@ const Users = () => {
   ]);
   return (
     <div className='users-page'>
-      <h1>CRUD Operation</h1>
-      <DataTable value={products} tableStyle={{ minWidth: '50rem' }}>
-        <Column field='code' header='Code'></Column>
-        <Column field='name' header='Name'></Column>
-        <Column field='category' header='Category'></Column>
-        <Column field='quantity' header='Quantity'></Column>
-      </DataTable>
+      <div className='container'>
+        <h1>CRUD Operation</h1>
+        <div className='users-list'>
+          <DataTable value={products}>
+            <Column field='code' header='Code'></Column>
+            <Column field='name' header='Name'></Column>
+            <Column field='category' header='Category'></Column>
+            <Column field='quantity' header='Quantity'></Column>
+          </DataTable>
+        </div>
+      </div>
     </div>
   );
 };
